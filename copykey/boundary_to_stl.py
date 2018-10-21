adf51d00fd0158f4c8980c127b1c051fb218c112
Kevin Black <kevin3.black@gmail.com>
2018-10-21 04:04:49 -0500

    Add baseline rotation stuff

---
+++ copykey/boundary_to_stl.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import visvalingamwyatt as vw
+
+def align(boundary):
+    """
+    Rotates the boundary based on the baseline. Returns the rotated boundary and the new
+    y-value of the baseline.
+    """
+    tip_index = np.argmax(boundary[:, 0])
+    tip = boundary[tip_index]
+
+    boundary_bottom = boundary[tip_index:]
+
+    # between 15 and 5 mm back from tip
+    baseline_start = tip[0] - 15
+    baseline_end = tip[0] - 5
+    baseline = boundary_bottom[np.logical_and(boundary_bottom[:, 0] >= baseline_start, boundary_bottom[:, 0] <= baseline_end)]
+
+    # shift baseline to origin
+    baseline_origin = baseline - baseline[-1]
+    slope = np.linalg.lstsq(baseline_origin[:, 0].reshape(-1, 1), baseline_origin[:, 1], rcond=None)[0]
+    angle = -1 * np.asscalar(np.arctan(slope))
+    rotation_matrix = np.array([[np.cos(angle), -1 * np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+
+    boundary_rotated = rotation_matrix.dot(boundary.T).T
+    baseline_rotated = rotation_matrix.dot(baseline.T).T
+    return boundary_rotated, np.mean(baseline_rotated[:, 1])
+    # return baseline[0], baseline[-1]
+
+def boundary_to_stl(boundary):
+    boundary_rotated, baseline = align(boundary)
+    return boundary_rotated
+
